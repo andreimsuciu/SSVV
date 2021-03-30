ac79f08e30d26f8c003c0fa1dbf9aa2865852da1
andreimsuciu <andreisuciu@hotmail.com>
2021-03-30 11:55:39 +0300

    tests added and validator updated

--- src/main/java/Validator/StudentValidator.java
+++ src/main/java/Validator/StudentValidator.java
@@ -2,11 +2,16 @@ package Validator;
 import Exceptions.ValidatorException;
 import Domain.Student;
 
+import java.util.regex.Pattern;
+
 public class StudentValidator implements IValidator<Student> {
 
     public void validate(Student s) throws ValidatorException {
+        String regex="^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+        Pattern pattern = Pattern.compile(regex);
+
         String errors="";
-        if(s.getId().equals("")){
+        if(s.getId().equals("") || s.getId() == null){
             //throw new ValidatorException("Id invalid\n");
             errors+="Id invalid\n";
         }
@@ -14,11 +19,15 @@ public class StudentValidator implements IValidator<Student> {
             //throw new ValidatorException("Nume invalid\n");
             errors+="Nume invalid\n";
         }
+        if(s.getIndrumator().equals("") || s.getIndrumator()==null){
+            //throw new ValidatorException("Nume invalid\n");
+            errors+="Nume invalid\n";
+        }
         if(s.getGrupa()<=0){
             //throw new ValidatorException("Grupa invalida\n");
             errors+="Grupa invalid\n";
         }
-        if(s.getEmail().equals("") || s.getEmail()==null){
+        if(s.getEmail().equals("") || s.getEmail()==null || !pattern.matcher(s.getEmail()).matches()){
             //throw new ValidatorException("Email invalid\n");
             errors+="Email invalid\n";
         }
--- src/test/java/ssvv/dreamteam/MainTest.java
+++ src/test/java/ssvv/dreamteam/MainTest.java
@@ -24,7 +24,7 @@ public class MainTest {
         StudentValidator vs=new StudentValidator();
         StudentXMLRepo strepo=new StudentXMLRepo(vs,"StudentiXML.xml");
         StudentXMLService studentXMLService = new StudentXMLService(strepo);
-        String[] params={"1","nume","grupa","email","prof"};
+        String[] params={"1","nume","grupa","nume@gmail.com","prof"};
         try{
             studentXMLService.add(params);
         }catch (ValidatorException ex){
@@ -32,13 +32,22 @@ public class MainTest {
         }
         assertNull(studentXMLService.findOne("1"));
 
-        String[] params2={"1","nume","2","email","prof"};
+        String[] params2={"1","nume","2","nume@gmail.com","prof"};
         try{
             studentXMLService.add(params2);
         }catch (ValidatorException ex){
             System.out.println(ex.getMessage());
         }
         assertNotNull(studentXMLService.findOne("1"));
+        studentXMLService.remove("1");
+
+        String[] params3={"2","nume","-1","nume@gmail.com","prof"};
+        try{
+            studentXMLService.add(params3);
+        }catch (ValidatorException ex){
+            System.out.println(ex.getMessage());
+        }
+        assertNull(studentXMLService.findOne("1"));
         studentXMLService.remove("1");
     }
 
@@ -47,16 +56,16 @@ public class MainTest {
         StudentValidator vs=new StudentValidator();
         StudentXMLRepo strepo=new StudentXMLRepo(vs,"StudentiXML.xml");
         StudentXMLService studentXMLService = new StudentXMLService(strepo);
-        String[] params={"1","nume","2","email","prof"};
+        String[] params={"1","nume","2","nume@gmail.com","prof"};
         try{
             studentXMLService.add(params);
         }catch (ValidatorException ex){
             System.out.println(ex.getMessage());
         }
 
-        String[] params2={"1","nume2","2","email2","prof2"};
+        String[] params2={"1","nume2","2","nume@gmail.com","prof2"};
         try{
-            studentXMLService.add(params);
+            studentXMLService.add(params2);
         }catch (ValidatorException ex){
             System.out.println(ex.getMessage());
         }
@@ -65,7 +74,148 @@ public class MainTest {
         Student student = studentXMLService.findOne("1");
         assertEquals("1",student.getId());
         assertEquals("nume",student.getNume());
-        assertEquals("email",student.getEmail());
+        assertEquals("nume@gmail.com",student.getEmail());
         assertEquals("prof",student.getIndrumator());
+
+        boolean fails=false;
+        String[] params3={"","nume2","2","nume@gmail.com","prof2"};
+        try{
+            studentXMLService.add(params3);
+        }catch (ValidatorException ex){
+            System.out.println(ex.getMessage());
+            fails=true;
+        }
+        assertTrue(fails);
+
+        fails=false;
+        String[] params4={null,"nume2","2","nume@gmail.com","prof2"};
+        try{
+            studentXMLService.add(params4);
+        }catch (ValidatorException | NullPointerException ex){
+            System.out.println(ex.getMessage());
+            fails=true;
+        }
+        assertTrue(fails);
+    }
+
+    @Test
+    public void testAddStudentName(){
+        StudentValidator vs=new StudentValidator();
+        StudentXMLRepo strepo=new StudentXMLRepo(vs,"StudentiXML.xml");
+        StudentXMLService studentXMLService = new StudentXMLService(strepo);
+
+        boolean fails=false;
+        String[] params={"1","nume","2","nume@gmail.com","prof"};
+        try{
+            studentXMLService.add(params);
+        }catch (ValidatorException ex){
+            System.out.println(ex.getMessage());
+            fails=true;
+        }
+        assertFalse(fails);
+
+        fails=false;
+        String[] params2={"2","","2","nume@gmail.com","prof2"};
+        try{
+            studentXMLService.add(params2);
+        }catch (ValidatorException ex){
+            System.out.println(ex.getMessage());
+            fails=true;
+        }
+        assertTrue(fails);
+
+        fails=false;
+        String[] params3={"2",null,"2","nume@gmail.com","prof2"};
+        try{
+            studentXMLService.add(params3);
+        }catch (NullPointerException | ValidatorException ex){
+            System.out.println(ex.getMessage());
+            fails=true;
+        }
+        assertTrue(fails);
+    }
+
+    @Test
+    public void testAddStudentProf(){
+        StudentValidator vs=new StudentValidator();
+        StudentXMLRepo strepo=new StudentXMLRepo(vs,"StudentiXML.xml");
+        StudentXMLService studentXMLService = new StudentXMLService(strepo);
+
+        boolean fails=false;
+        String[] params={"1","nume","2","nume@gmail.com","prof"};
+        try{
+            studentXMLService.add(params);
+        }catch (ValidatorException ex){
+            System.out.println(ex.getMessage());
+            fails=true;
+        }
+        assertFalse(fails);
+
+        fails=false;
+        String[] params2={"2","nume","2","nume@gmail.com",""};
+        try{
+            studentXMLService.add(params2);
+        }catch (ValidatorException ex){
+            System.out.println(ex.getMessage());
+            fails=true;
+        }
+        assertTrue(fails);
+
+        fails=false;
+        String[] params3={"2","nume","2","nume@gmail.com",null};
+        try{
+            studentXMLService.add(params3);
+        }catch (ValidatorException | NullPointerException ex){
+            System.out.println(ex.getMessage());
+            fails=true;
+        }
+        assertTrue(fails);
+    }
+
+    @Test
+    public void testAddStudentEmail(){
+        StudentValidator vs=new StudentValidator();
+        StudentXMLRepo strepo=new StudentXMLRepo(vs,"StudentiXML.xml");
+        StudentXMLService studentXMLService = new StudentXMLService(strepo);
+
+        boolean fails=false;
+        String[] params={"1","nume","2","nume@gmail.com","prof"};
+        try{
+            studentXMLService.add(params);
+        }catch (ValidatorException ex){
+            System.out.println(ex.getMessage());
+            fails=true;
+        }
+        assertFalse(fails);
+
+        fails=false;
+        String[] params2={"2","nume","2","email","prof"};
+        try{
+            studentXMLService.add(params2);
+        }catch (ValidatorException ex){
+            System.out.println(ex.getMessage());
+            fails=true;
+        }
+        assertTrue(fails);
+
+        fails=false;
+        String[] params3={"2","nume","2","","prof"};
+        try{
+            studentXMLService.add(params3);
+        }catch (ValidatorException ex){
+            System.out.println(ex.getMessage());
+            fails=true;
+        }
+        assertTrue(fails);
+
+        fails=false;
+        String[] params4={"2","nume","2",null,"prof"};
+        try{
+            studentXMLService.add(params4);
+        }catch (ValidatorException | NullPointerException ex){
+            System.out.println(ex.getMessage());
+            fails=true;
+        }
+        assertTrue(fails);
     }
 }
